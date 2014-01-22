8fa785d2398e62b68df2ea8e8e1aaeee448fdf04
mrzl <lzrm@mrzl.net>
2014-01-23 00:56:40 +0100

    adds some comments
    Signed-off-by: mrzl <lzrm@mrzl.net>

--- src/main/java/goeuro/application/GeoEuroTestMain.java
+++ src/main/java/goeuro/application/GeoEuroTestMain.java
@@ -8,17 +8,29 @@ package goeuro.application;
  */
 public class GeoEuroTestMain {
 
+    /**
+     * This method prints an error message to the error console and exits.
+     *
+     * @param errorMessage the message which is being printed to the error console before the program quits.
+     */
     public static void exitWithErrorMessage( String errorMessage ) {
         System.err.println( errorMessage );
         System.exit( 1 );
     }
 
+    /**
+     * This is the entry point of the program- main function.
+     *
+     * @param args should have only one parameter. the city name of which the information is to be retrieved.
+     */
     public static void main( String[] args ) {
         String cityName = "";
         try {
+            // parsing the command line parameters and returning the string of the city name
             cityName = CommandLineParser.parseCommandLineInput( args );
         } catch ( Exception e ) {
-            exitWithErrorMessage( "Couldn't parse command line input." );
+            // if the command line parsing went wrong, print the message and exit
+            exitWithErrorMessage( e.getMessage() );
         }
 
 
@@ -31,5 +43,10 @@ public class GeoEuroTestMain {
         }
 
         JSONParser parser = new JSONParser( jsonString );
+
+        String exportedCsvFileName = cityName + ".csv";
+        CsvExporter csvExport = new CsvExporter( exportedCsvFileName );
+        csvExport.exportCsv( parser.getParsedFields() );
+
     }
 }
