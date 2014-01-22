8ad6f1022d1281b15a87c3c6304cfc2b8bad1dac
mrzl <lzrm@mrzl.net>
2014-01-23 00:56:07 +0100

    implements the csv exporter class
    Signed-off-by: mrzl <lzrm@mrzl.net>

--- src/main/java/goeuro/application/CsvExporter.java
+++ src/main/java/goeuro/application/CsvExporter.java
@@ -1,5 +1,7 @@
 package goeuro.application;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -10,9 +12,46 @@ import java.util.ArrayList;
  */
 public class CsvExporter {
 
-    private ArrayList< Field > fieldsToExport;
+    private String delimiter, exportedCsvFileName;
 
-    public CsvExporter( ArrayList<Field> fieldsToExport ) {
-        this.fieldsToExport = fieldsToExport;
+    public CsvExporter( String exportedCsvFileName ) {
+        this.exportedCsvFileName = exportedCsvFileName;
+        // delimiter needs to be a semicolon, because the field 'name' contains commas
+        this.delimiter = ";";
+    }
+
+    public void exportCsv( ArrayList<Field> fieldsToExport ) {
+        try {
+            FileWriter writer = new FileWriter( this.exportedCsvFileName );
+            writeCsvHeaderToFile( writer );
+
+            for ( Field f : fieldsToExport ) {
+                writeFieldToFile( writer, f );
+            }
+
+            writer.flush();
+            writer.close();
+        } catch ( IOException e ) {
+            e.printStackTrace();
+        } finally {
+            System.out.println( "Sucessfully exported " + fieldsToExport.size() + " fields to " + this.exportedCsvFileName + "." );
+        }
+
+    }
+
+    private void writeCsvHeaderToFile( FileWriter writer ) throws IOException {
+        writer.append( "_type" + this.delimiter + "_id" + this.delimiter + "name" + this.delimiter + "type" + this.delimiter + "latitude" + this.delimiter + "longitude" + System.lineSeparator() );
+    }
+
+    private void writeFieldToFile( FileWriter writer, Field field ) throws IOException {
+        String lineToAdd =
+                field.get_type() + this.delimiter +
+                        field.getId() + this.delimiter +
+                        field.getName() + this.delimiter +
+                        field.getType() + this.delimiter +
+                        field.getLatitude() + this.delimiter +
+                        field.getLongitude() + System.lineSeparator();
+
+        writer.append( lineToAdd );
     }
 }
