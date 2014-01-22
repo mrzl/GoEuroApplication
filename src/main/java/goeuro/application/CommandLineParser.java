c4888b37c976b5efc3fba9a99116861f44a2b7d2
mrzl <lzrm@mrzl.net>
2014-01-23 00:55:53 +0100

    adds some regular expression check in order to avoid wrong user input
    Signed-off-by: mrzl <lzrm@mrzl.net>

--- src/main/java/goeuro/application/CommandLineParser.java
+++ src/main/java/goeuro/application/CommandLineParser.java
@@ -10,10 +10,15 @@ public class CommandLineParser {
     // TODO: check per regex if city name contains only letters
 
     public static String parseCommandLineInput( String[] args ) throws Exception {
-        if ( args.length != 1 ) {
-            throw new Exception( "Can't parse input." );
-        } else {
-            return args[ 0 ];
+        if ( args.length == 1 ) {
+            String pattern = "[a-zA-z]+";
+            String cityName = args[ 0 ];
+            if ( cityName.matches( pattern ) ) {
+                return args[ 0 ];
+            } else {
+                throw new Exception( "Please pass a valid city name as a parameter. Aborting." );
+            }
         }
+        throw new Exception( "Input parameters not correctly specified. Please only specify one parameter. Aborting." );
     }
 }
