d468e8d05dfa79c59bd770a83414f03cf4a8e977
mrzl <lzrm@mrzl.net>
2014-01-23 00:03:25 +0100

    adds a class to hold a field of the json parser
    Signed-off-by: mrzl <lzrm@mrzl.net>

---
+++ src/main/java/goeuro/application/Field.java
@@ -0,0 +1,62 @@
+package goeuro.application;
+
+/**
+ * Author: mrzl
+ * Date: 22.01.14
+ * Time: 23:14
+ * Project: GoEuroTest
+ */
+public class Field {
+
+    private long id;
+    private String _type, name, type;
+    private double[] geo_position;
+
+    public Field( String _type, long id, String name, String type, double[] geo_position ) {
+        this._type = _type;
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.geo_position = geo_position;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId( long id ) {
+        this.id = id;
+    }
+
+    public String get_type() {
+        return _type;
+    }
+
+    public void set_type( String _type ) {
+        this._type = _type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName( String name ) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType( String type ) {
+        this.type = type;
+    }
+
+    public double[] getGeoPosition() {
+        return geo_position;
+    }
+
+    public void setGeoPosition( double[] geo_position ) {
+        this.geo_position = geo_position;
+    }
+}
