ee88be494e9fbe062f536b1ee0cfbe8ecac9a359
mrzl <lzrm@mrzl.net>
2014-01-25 21:10:13 +0100

    adds lots of nice comments
    Signed-off-by: mrzl <lzrm@mrzl.net>

--- src/main/java/goeuro/application/CsvExporter.java
+++ src/main/java/goeuro/application/CsvExporter.java
@@ -9,43 +9,77 @@ import java.util.ArrayList;
  * Date: 23.01.14
  * Time: 00:01
  * Project: GoEuroTest
+ *
+ * This class exports data to a CSV file. It's adopted to work with special GoEuro data.
  */
 public class CsvExporter {
-
+    // some Strings which specify the selected delimiter and the file name
     private String delimiter, exportedCsvFileName;
 
+    /**
+     * The constructor of the CSV file exporter. It's not doing a lot except from
+     * setting a couple of global variables
+     *
+     * @param exportedCsvFileName a {@link java.lang.String} that sets the file name of the exported CSV file
+     */
     public CsvExporter( String exportedCsvFileName ) {
         this.exportedCsvFileName = exportedCsvFileName;
         // delimiter needs to be a semicolon, because the field 'name' contains commas
         this.delimiter = ";";
     }
 
-    public void exportCsv( ArrayList<Field> fieldsToExport ) throws IOException {
+    /**
+     * This is the core of the class. it takes an {@link java.util.ArrayList} of {@link goeuro.application.CityInformationField}
+     * and saves the content of this list into a CSV file with the specified file name and the specified delimiter
+     *
+     * @param fieldsToExport an {@link java.util.ArrayList} containing {@link goeuro.application.CityInformationField} which are being saved to the file
+     * @throws IOException if something with the writing goes wrong, it throws an {@link java.io.IOException}
+     */
+    public void exportCsv( ArrayList<CityInformationField> fieldsToExport ) throws IOException {
+        // creates a new FileWriter stating the file name
         FileWriter writer = new FileWriter( this.exportedCsvFileName );
+        // writes the header of the CSV file to the file
         writeCsvHeaderToFile( writer );
-
-        for ( Field f : fieldsToExport ) {
+        // loops through all passed fields and saves it to the file
+        for ( CityInformationField f : fieldsToExport ) {
             writeFieldToFile( writer, f );
         }
 
+        // actually flushes the data to the harddrive
         writer.flush();
+        // and closes the output
         writer.close();
 
         System.out.println( "Sucessfully exported " + fieldsToExport.size() + " fields to " + this.exportedCsvFileName + "." );
     }
 
+    /**
+     * Writes a typical header to the CSV file.
+     *
+     * @param writer a {@link java.io.FileWriter} the writer, where the header is to be written to
+     *
+     * @throws IOException if something goes wrong, an exception is thrown
+     */
     private void writeCsvHeaderToFile( FileWriter writer ) throws IOException {
         writer.append( "_type" + this.delimiter + "_id" + this.delimiter + "name" + this.delimiter + "type" + this.delimiter + "latitude" + this.delimiter + "longitude" + System.lineSeparator() );
     }
 
-    private void writeFieldToFile( FileWriter writer, Field field ) throws IOException {
+    /**
+     * Writes one line of data to the CSV file. This data is being specified by one {@link goeuro.application.CityInformationField} object
+     *
+     * @param writer the {@link java.io.FileWriter} where the field is being written to
+     * @param cityInformationField an instance representing the data of one line of the CSV file.
+     *
+     * @throws IOException if something goes wrong, an {@link java.io.IOException} is being thrown
+     */
+    private void writeFieldToFile( FileWriter writer, CityInformationField cityInformationField ) throws IOException {
         String lineToAdd =
-                field.get_type() + this.delimiter +
-                        field.getId() + this.delimiter +
-                        field.getName() + this.delimiter +
-                        field.getType() + this.delimiter +
-                        field.getLatitude() + this.delimiter +
-                        field.getLongitude() + System.lineSeparator();
+                cityInformationField.get_type() + this.delimiter +
+                        cityInformationField.getId() + this.delimiter +
+                        cityInformationField.getName() + this.delimiter +
+                        cityInformationField.getType() + this.delimiter +
+                        cityInformationField.getLatitude() + this.delimiter +
+                        cityInformationField.getLongitude() + System.lineSeparator();
 
         writer.append( lineToAdd );
     }
